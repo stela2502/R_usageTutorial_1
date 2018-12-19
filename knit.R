cd753c7018f4c701b3f2c35d0baa520c9e1b118a
stefanlang <Stefan.Lang@med.lu.se>
2018-12-19 09:43:47 +0100

    small update

--- knit.R
+++ knit.R
@@ -1,6 +1,6 @@
 require(knitr)
 require(rmarkdown)
-requite(tools)
+require(tools)
 fname = "index.Rmd"
 if ( file_ext(fname) == 'Rmd' ){
    ofile = paste(tools::file_path_sans_ext(fname), 'html', sep='.' ) 

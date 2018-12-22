c5e3d00e73df8fc96809ebef255390debad373f9
stela2502 <Stefan.Lang@med.lu.se>
2018-12-22 10:24:27 +0100

    almost done

---
+++ data/genecode_2_gene_id_gene_name_gtf.pl
@@ -0,0 +1,35 @@
+#! /usr7bin/perl -w
+
+use strict;
+use warnings;
+
+#problems:
+#gencode.vM9.annotation.gff3:chrM	ENSEMBL	exon	15356	15422	.	-	.	ID=exon:ENSMUST00000082423.1:1;Parent=ENSMUST00000082423.1;gene_id=ENSMUSG00000064372.1;transcript_id=ENSMUST00000082423.1;gene_type=Mt_tRNA;gene_status=KNOWN;gene_name=mt-Tp;transcript_type=Mt_tRNA;transcript_status=KNOWN;transcript_name=mt-Tp-201;exon_number=1;exon_id=ENSMUSE00000521550.1;level=3;transcript_support_level=NA;tag=basic
+
+my ($geneid, $seen, @tmp);
+print "gene_id\tgene_name\tgene_type\tchr\tstart\tend\ttransStart\torientation\n";
+while(my $line = <> ) { 
+	if (  $geneid = &match_and_ret($line,'gene_id') ){ #.*gene_name=([\w\.\_\-]+);/) {
+		next if ( $seen -> { $geneid } );
+		$seen -> { $geneid } =1;
+		print "$geneid";
+		foreach ( 'gene_name', 'gene_type' ) {
+			print "\t".&match_and_ret($line,$_);
+		}
+		@tmp = split("\t", $line);
+		print "\t". join("\t",@tmp[0,3,4]);
+		print "\t$tmp[3]" if ( $tmp[6] eq '+');
+		print "\t$tmp[4]" if ( $tmp[6] eq '-');
+		print "\t$tmp[6]\n";
+	} 
+}
+
+
+sub match_and_ret{
+	my ($str, $tag) = @_;
+	# gene_id "ENSMUSG00000102693.1";
+	if ( $str =~ m/$tag "([\w\.\-]+)";/){ 
+		return $1;
+	}
+	return '';
+}
